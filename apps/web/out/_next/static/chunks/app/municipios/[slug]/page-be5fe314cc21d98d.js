2d76b69c625a38ae78ba1be6e88191a8ddcc9605
Danilo <danilosfvalim@gmail.com>
2026-09-17 18:30:02 +0000

    Add .gitignore to exclude build artifacts from version control

--- apps/web/out/_next/static/chunks/app/municipios/[slug]/page-be5fe314cc21d98d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[27],{3974:(e,s,n)=>{Promise.resolve().then(n.bind(n,4257)),Promise.resolve().then(n.bind(n,9652)),Promise.resolve().then(n.bind(n,8508)),Promise.resolve().then(n.t.bind(n,9664,23))}},e=>{e.O(0,[664,576,371,587,18,358],()=>e(e.s=3974)),_N_E=e.O()}]);
